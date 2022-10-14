0b746341bdfd643a4f010d64f10dc82f5a14a71f
AhmetIOS <105635443+AhmetIOS@users.noreply.github.com>
2022-10-14 07:15:13 +0300

    Initial Commit

---
+++ Aşk Sözleri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aşk Sözleri
+//
+//  Created by Ahmet Durmuş on 14.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
